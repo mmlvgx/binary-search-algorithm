372ae534b9f11dbce4f888088ddb3648b6d6cebe
mmlvgx <mmlvgx@gmail.com>
2023-04-04 10:12:15 +0300

    Python

---
+++ python/main.py
@@ -0,0 +1,30 @@
+from math import floor
+
+
+def find(_list__: list[str], target: str, start: int, end: int) -> None:
+
+    if start > end:
+        # Not Found
+        return None
+
+    middle = floor((start + end) / 2)
+
+    if _list__[middle] == target:
+        # Found at middle of the list
+        return middle
+
+    # Recursion
+    elif _list__[middle] > target:
+        # Search left
+        return find(_list__, target, start, end - 1)
+    else:
+        # Search right
+        return find(_list__, target, start + 1, end)
+
+# Example
+list__ = ['a', 'b', 'c', 'x', 'y', 'z']
+
+element = find(list__, 'c', 0, 6)
+
+print(element)
+# >>> 2
